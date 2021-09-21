67d54bfa450f97d63be5e3fea852ad289c2d878f
Eduardo Alvear <ealvear0312@live.com>
2021-09-20 23:39:49 -0500

    Punto 1

--- ejercicios_ciclo.py
+++ ejercicios_ciclo.py
@@ -1,7 +1,31 @@
-# -*- coding: utf-8 -*-
 """
-Created on Mon Sep 20 23:33:25 2021
 
 @author: Eduardo Alvear
 """
 
+def punto1(carros):
+    c_amarillo = 0
+    c_rosa = 0
+    c_roja = 0
+    c_verde = 0
+    c_azul = 0
+
+    for auto in carros:
+        if auto == 1 or auto == 2:
+            c_amarillo += 1
+        elif auto == 3 or auto == 4:
+            c_rosa += 1
+        elif auto == 5 or auto == 6:
+            c_roja += 1
+        elif auto == 7 or auto == 8:
+            c_verde += 1
+        elif auto == 9 or auto == 0:
+            c_azul += 1
+
+    return {
+        "Autos amarillos": c_amarillo,
+        "Autos rosa": c_rosa,
+        "Autos rojos": c_roja,
+        "Autos verdes": c_verde,
+        "Autos azules": c_azul
+    }
