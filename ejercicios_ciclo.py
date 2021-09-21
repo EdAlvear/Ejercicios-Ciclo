1fc0b838e5b60edc53ca00eddfd0b1a3f359df46
Eduardo Alvear <ealvear0312@live.com>
2021-09-20 23:51:09 -0500

    Punto 2

--- ejercicios_ciclo.py
+++ ejercicios_ciclo.py
@@ -31,3 +31,49 @@ def punto1(carros):
     }
 
 
+def punto2(animal):
+    edad_animales = []
+    if animal.lower() == 'elefante':
+        for i in range(20):
+            elefante = int(input('Digite la edad del elefante:'))
+            edad_animales.append(elefante)
+    if animal.lower() == 'jirafa':
+        for i in range(15):
+            jirafa = int(input('Digite edad de la jirafa:'))
+            edad_animales.append(jirafa)
+    if animal.lower() == 'chimpance':
+        for i in range(40):
+            chimpance = int(input('Digite la edad del chimpance:'))
+            edad_animales.append(chimpance)
+    
+    c1 = c2 = c3 = 0
+    for edad in edad_animales:
+        if edad >= 0 and edad <= 1:
+            c1 += 1
+        elif edad > 1 and edad < 3:
+            c2 += 1
+        elif edad >= 3:
+            c3 += 1
+    
+    if animal.lower() == 'elefante':
+        c1 = (c1 * 100) / 20 
+        c2 = (c2 * 100) / 20
+        c3 = (c3 * 100) / 20
+        
+    if animal.lower() == 'jirafa':
+        c1 = (c1 * 100) / 15 
+        c2 = (c2 * 100) / 15
+        c3 = (c3 * 100) / 15
+        
+    if animal.lower() == 'chimpance':
+        c1 = (c1 * 100) / 40
+        c2 = (c2 * 100) / 40
+        c3 = (c3 * 100) / 40
+
+
+    return {
+        "animal": animal,
+        "Entre 0 y 1 año": c1,
+        "Entre 1 y 3 años": c2,
+        "Mayor o igual a 3 años": c3
+    }
